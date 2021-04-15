2482014a886f364670d69ddd97625474d364588a
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-04-15 17:45:29 -0400

    factor.hs: Some unnecessarily parens are deleted.

--- factor.hs
+++ factor.hs
@@ -7,7 +7,7 @@ printFactors :: Integer -> IO ();
 printFactors k = putStr (show k ++ ": ") >> showTheNums
   where
     listNums = filter (\a -> mod k a == 0) thePrimes
-    showTheNums = mapM_ (\ a -> putStr $ (show a) ++ " ") listNums >> putStrLn "";
+    showTheNums = mapM_ (\ a -> putStr $ show a ++ " ") listNums >> putStrLn "";
 
 main :: IO ();
 main = getLine >>= printFactors . reed >> main;

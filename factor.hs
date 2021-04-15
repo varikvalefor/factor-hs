6361477dafd63899f1b3507e1b0ae99d0271f777
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-04-15 17:42:06 -0400

    Create factor.hs

---
+++ factor.hs
@@ -0,0 +1,13 @@
+import ThePrimes;
+
+reed :: String -> Integer;
+reed k = read k :: Integer;
+
+printFactors :: Integer -> IO ();
+printFactors k = putStr (show k ++ ": ") >> showTheNums
+  where
+    listNums = filter (\a -> mod k a == 0) thePrimes
+    showTheNums = mapM_ (\ a -> putStr $ (show a) ++ " ") listNums >> putStrLn "";
+
+main :: IO ();
+main = getLine >>= printFactors . reed >> main;

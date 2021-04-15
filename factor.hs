66fb728d7a73b3880d9b4166dbff2a5cf0871329
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-04-15 17:48:50 -0400

    factor.hs: The length of a line decreases.

--- factor.hs
+++ factor.hs
@@ -7,7 +7,7 @@ printFactors :: Integer -> IO ();
 printFactors k = putStr (show k ++ ": ") >> showNums
   where
     listNums = filter ((==0) . mod k) thePrimes
-    showNums = mapM_ (\ a -> putStr $ show a ++ " ") listNums >> putStrLn "";
+    showNums = mapM_ (putStr . (++ " ") . show) listNums >> putStrLn "";
 
 main :: IO ();
 main = getLine >>= printFactors . reed >> main;

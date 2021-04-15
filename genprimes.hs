41a67501248aa3958e0fa05342d11e87e82dacb3
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-04-15 17:42:43 -0400

    Create genprimes.hs

---
+++ genprimes.hs
@@ -0,0 +1,12 @@
+import System.Environment
+import Data.List;
+
+isPrime :: Integer -> Bool;
+isPrime k = not $ any (\a -> mod k a == 0) [2..squirt k - 1];
+
+squirt :: Integer -> Integer;
+squirt = floor . sqrt . fromIntegral;
+
+main :: IO ();
+main = getArgs >>= return . (!!0) >>= \ a ->
+  mapM_ putStr $ intersperse "," $ map show $ filter isPrime [2..10^(read a :: Integer)];

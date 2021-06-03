949426062316ea72fdd137db00cbfe30258854b4
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-06-02 20:36:23 -0400

    factor.hs: Documentation is added.

--- factor.hs
+++ factor.hs
@@ -1,10 +1,16 @@
 import ThePrimes;
 
+-- | For all Integer k, printFactors k prints the space-separated
+-- prime factors of k to the terminal.
 printFactors :: Integer -> IO ();
 printFactors k = putStr (show k ++ ": ") >> showNums
   where
     listFacs = filter ((==0) . mod k) thePrimes
     showNums = mapM_ (putStr . (++ " ") . show) listFacs >> putStrLn "";
 
+-- | main is the main program loop.
+-- For all input String k, if k can be parsed as a value of type Integer,
+-- then the factors of the Integer version of k are printed to the
+-- terminal.
 main :: IO ();
 main = getLine >>= printFactors . read >> main;

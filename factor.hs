5c70b76b868bbe4ca86ddb5cb63012f2c58091b4
Varik Valefor <varikvalefor@aol.com>
2021-08-31 17:38:47 -0400

    factor.hs: Documentation is added.

--- factor.hs
+++ factor.hs
@@ -1,3 +1,5 @@
+import ThePrimes;
+
 main :: IO ();
 main = getLine >>= prt . read >> main
   where
@@ -6,6 +8,8 @@ main = getLine >>= prt . read >> main
     | product (primeFactors n) /= n = error "primeFactors is wrong!!!"
     | otherwise = putStrLn $ concat [show n, ": ", unwords (map show $ primeFactors n)];
 
+-- | @primeFactors k@ is a list of numbers @j@ such that
+-- @product j == k@ and @j@ is a subset of the prime numbers.
 primeFactors :: Integer -> [Integer];
 primeFactors n
   | f == [n] = f
@@ -14,6 +18,10 @@ primeFactors n
   f :: [Integer]
   f = factors n;
 
+-- | If @n@ is a composite number, then @factors n@ is a 2-list whose
+-- product equals @n@.
+--
+-- If @n@ is prime, then @factors n == [n]@.
 factors :: Integer -> [Integer]
 factors n
   | f == [1] = [n]
@@ -22,6 +30,7 @@ factors n
   f :: [Integer]
   f = filter ((==0) . (n `mod`)) [1..isqrt n];
 
+-- | @isqrt n@ is the integer square root of @n@.
 isqrt :: Integer -> Integer;
 isqrt n 
   | n <= fromIntegral (maxBound :: Int) = floor $ sqrt $ fromIntegral n + 0.0

ec8f0f829b9fc7035bdbb83f5062dfa4d7235114
Varik Valefor <varikvalefor@aol.com>
2022-03-31 18:35:08 -0400

    isPrime, toFactorTriplet, and toFactorTriplets are added.

--- src/factor.hs
+++ src/factor.hs
@@ -1,4 +1,8 @@
+import Data.Bool;
+import Data.List;
+import Data.Maybe;
 import Data.List.Extra;
+import qualified Data.Set as S;
 
 main :: IO ();
 main = getLine >>= prt . read >> main
@@ -22,6 +26,50 @@ primeFactors n = maybe [n] (concatMap primeFactors) $ factors n;
 -- If @n@ is prime, then @factors n == 'Nothing'@.
 factors :: Integer -> Maybe [Integer]
 factors n = (\d -> [d, n `div` d]) <$> firstNonUnityFactor n;
+
+-- | @isPrime k@ iff @k@ is prime.
+isPrime :: Integer -> Bool;
+isPrime = isNothing . factors;
+
+-- | If @n@ is prime, then @toFactorTriplet n@ is 'Nothing'.
+-- @toFactorTriplet n@ is otherwise 'Just' a 3-tuple @(a,b,c)@
+-- such that @a == n@ and @a == b * c@.
+toFactorTriplet :: Integer -> Maybe (Integer, Integer, Integer);
+toFactorTriplet n = (\[a,b] -> (n,a,b)) <$> factors n;
+
+-- | Where \(k\) is any 'Integer' and \(t\) denotes
+-- @toFactorTriplets k@...
+--
+-- \[
+--   \forall u \in t,\ 
+--   t_1 = t_2 \cdot t_3.
+-- \]
+--
+-- \[
+--   \max \left\{a_0 : a \in t\right\} = k.
+-- \]
+--
+-- \[
+--   \forall n \in t,\ 
+--   \forall m \in \left\{2,3\right\},\ 
+--   \left(
+--     n_m \in \mathbb P
+--   \right) \veebar
+--   \left(
+--     \left\{j \in t : j_1 = n_m\right\} \neq \left\{\right\}
+--   \right).
+-- \]
+--
+-- \[
+--   \forall n \in t,\ 
+--   \left\{1, k\right\} \cap t = \left\{\right\}.
+-- \]
+toFactorTriplets :: Integer -> S.Set (Integer, Integer, Integer);
+toFactorTriplets = fl . recurse . catMaybes . pure . toFactorTriplet
+  where
+  fl = S.fromList
+  recurse l = bool (recurse $ l ++ newTriplets l) l $ null $ newTriplets l
+  newTriplets l = catMaybes $ map toFactorTriplet $ filter (\a -> filter (\(t,_,_) -> t == a) l == [] && not (isPrime a)) $ concat $ map (\(a,b,c) -> [b,c]) l
 
 -- | If @n@ is prime, then @firstNonUnityFactor n@ is 'Nothing'.
 --
